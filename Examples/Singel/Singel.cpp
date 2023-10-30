b7567ba0fc6f57946733bb1a4f53d2a264d00c49
Shah <aashay.as99@gmail.com>
2023-10-30 14:42:08 -0700

    Formatting with clang-format

--- Examples/Singel/Singel.cpp
+++ Examples/Singel/Singel.cpp
@@ -4,34 +4,28 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "lib/VL53L0X/VL53L0X.hpp"
-
+#include "pico/stdlib.h"
+#include <stdio.h>
 
 VL53L0X sensor;
-void init_VL53L0X()
-{
+void init_VL53L0X() {
     sensor.init_i2c(0, 4, 5);
     sensor.setTimeout(500);
-    if (!sensor.init(true))
-    {
+    if (!sensor.init(true)) {
         printf("Failed to detect and initialize sensor!\n");
         return;
     }
 }
 
-int main()
-{
+int main() {
     stdio_init_all();
     init_VL53L0X();
-    while (true)
-    {
+    while (true) {
         uint16_t value = sensor.readRangeSingleMillimeters();
         printf("\n%i", value);
-        if (sensor.timeoutOccurred())
-        {
+        if (sensor.timeoutOccurred()) {
             printf(" TIMEOUT");
         }
 
